505d72638abf3b02a2bd4e4c007151593ae73452
FluxMusic <alex.huber743@gmail.com>
2025-04-17 22:12:16 +0200

    Wrote C++ GameMode to Play Music

---
+++ Source/ProceduralMusic/Private/ProceduralMusicGameMode.cpp
@@ -0,0 +1,118 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProceduralMusicGameMode.h"
+
+#include "DrumAndBassGenerator.h"
+#include "Components/AudioComponent.h"
+
+AProceduralMusicGameMode::AProceduralMusicGameMode()
+{
+    m_pDnBGenerator = NewObject<UDrumAndBassGenerator>(this, "DnB Generator");
+
+    m_pMusic = CreateDefaultSubobject<UAudioComponent>("Music");
+    m_pMusic->SetSound(ConstructorHelpers::FObjectFinder<USoundBase>(*META_SOUND_PATH).Object);
+    m_pMusic->SetAutoActivate(false);
+    m_pMusic->SetupAttachment(RootComponent);
+}
+
+void AProceduralMusicGameMode::BeginPlay()
+{
+    FDrumAndBassPatterns Patterns = m_pDnBGenerator->GenerateMusic(m_Seed, m_MusicGenSpecs);
+
+    SetDrumPatterns(Patterns.KickPattern, Patterns.SnarePattern, Patterns.HHPattern, Patterns.Perc1Pattern, Patterns.Perc2Pattern);
+    SetBassPatterns(Patterns.BassNotes);
+    SetChordPatterns(Patterns.Chords);
+    SetMelodyPatterns(Patterns.MelodyNotes);
+    
+    PlayMusic();
+}
+
+void AProceduralMusicGameMode::SetDrumPatterns(TArray<float> a_KickPattern, TArray<float> a_SnarePattern, TArray<float> a_HHPattern, TArray<float> a_Perc1Pattern, TArray<float> a_Perc2Pattern)
+{
+    m_pMusic->SetFloatArrayParameter(KICK_PATTERN_NAME, a_KickPattern);
+    m_pMusic->SetFloatArrayParameter(SNARE_PATTERN_NAME, a_SnarePattern);
+    m_pMusic->SetFloatArrayParameter(HH_PATTERN_NAME, a_HHPattern);
+    m_pMusic->SetFloatArrayParameter(PERC1_PATTERN_NAME, a_Perc1Pattern);
+    m_pMusic->SetFloatArrayParameter(PERC2_PATTERN_NAME, a_Perc2Pattern);
+}
+
+void AProceduralMusicGameMode::SetBassPatterns(TArray<FMusicNote> a_BassNotes)
+{
+    TArray<TArray<float>> BassPatterns = SplitNoteArray(a_BassNotes);
+
+    m_pMusic->SetFloatArrayParameter(BASS_NOTES_PATTERN_NAME, BassPatterns[0]);
+    m_pMusic->SetFloatArrayParameter(BASS_VELOCITIES_PATTERN_NAME, BassPatterns[1]);
+}
+
+void AProceduralMusicGameMode::SetChordPatterns(FChordArrays a_Chords)
+{
+    TArray<TArray<float>> RootNotes = SplitNoteArray(a_Chords.RootNotes);
+    TArray<TArray<float>> ThirdNotes = SplitNoteArray(a_Chords.ThirdNotes);
+    TArray<TArray<float>> FifthNotes = SplitNoteArray(a_Chords.FifthNotes);
+    TArray<TArray<float>> SeventhNotes = SplitNoteArray(a_Chords.SeventhNotes);
+    TArray<TArray<float>> NinthNotes = SplitNoteArray(a_Chords.NinthNotes);
+    TArray<TArray<float>> EleventhNotes = SplitNoteArray(a_Chords.EleventhNotes);
+    TArray<TArray<float>> ThirteenthNotes = SplitNoteArray(a_Chords.ThirteenthNotes);
+
+    m_pMusic->SetFloatArrayParameter(CHORD_ROOT_NOTES_PATTERN_NAME, RootNotes[0]);
+    m_pMusic->SetFloatArrayParameter(CHORD_ROOT_VELOCITIES_PATTERN_NAME, RootNotes[1]);
+    m_pMusic->SetFloatArrayParameter(CHORD_THIRD_NOTES_PATTERN_NAME, ThirdNotes[0]);
+    m_pMusic->SetFloatArrayParameter(CHORD_THIRD_VELOCITIES_PATTERN_NAME, ThirdNotes[1]);
+    m_pMusic->SetFloatArrayParameter(CHORD_FIFTH_NOTES_PATTERN_NAME, FifthNotes[0]);
+    m_pMusic->SetFloatArrayParameter(CHORD_FIFTH_VELOCITIES_PATTERN_NAME, FifthNotes[1]);
+    m_pMusic->SetFloatArrayParameter(CHORD_SEVENTH_NOTES_PATTERN_NAME, SeventhNotes[0]);
+    m_pMusic->SetFloatArrayParameter(CHORD_SEVENTH_VELOCITIES_PATTERN_NAME, SeventhNotes[1]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_NINTH_NOTES_PATTERN_NAME, NinthNotes[0]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_NINTH_VELOCITIES_PATTERN_NAME, NinthNotes[1]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_ELEVENTH_NOTES_PATTERN_NAME, EleventhNotes[0]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_ELEVENTH_VELOCITIES_PATTERN_NAME, EleventhNotes[1]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_THIRTEENTH_NOTES_PATTERN_NAME, ThirteenthNotes[0]);
+    // m_pMusic->SetFloatArrayParameter(CHORD_THIRTEENTH_VELOCITIES_PATTERN_NAME, ThirteenthNotes[1]);
+}
+
+void AProceduralMusicGameMode::SetMelodyPatterns(TArray<FMusicNote> a_MelodyNotes)
+{
+    TArray<TArray<float>> MelodyNotes = SplitNoteArray(a_MelodyNotes);
+
+    m_pMusic->SetFloatArrayParameter(MELODY_NOTES_PATTERN_NAME, MelodyNotes[0]);
+    m_pMusic->SetFloatArrayParameter(MELODY_VELOCITIES_PATTERN_NAME, MelodyNotes[1]);
+}
+
+TArray<TArray<float>> AProceduralMusicGameMode::SplitNoteArray(TArray<FMusicNote> a_NotePattern)
+{
+    TArray<TArray<float>> Patterns;
+
+    TArray<float> MidiNotes, Velocities;
+    MidiNotes.SetNum(a_NotePattern.Num());
+    Velocities.SetNum(a_NotePattern.Num());
+
+    for (size_t i = 0; i < a_NotePattern.Num(); i++)
+    {
+        MidiNotes[i] = a_NotePattern[i].MidiNote;
+        Velocities[i] = a_NotePattern[i].Velocity;
+    }
+
+    Patterns.Add(MidiNotes);
+    Patterns.Add(Velocities);
+    
+    return Patterns;
+}
+
+void AProceduralMusicGameMode::PlayMusic()
+{
+    if (!m_pMusic)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Audio Component for the music does not exist!"));
+        return;
+    }
+    if (!m_pMusic->GetSound())
+    {
+        UE_LOG(LogTemp, Error, TEXT("No Sound loaded to the Component!"));
+        return;
+    }
+    if (m_pMusic->IsActive()  == false) m_pMusic->SetActive(true);
+    if (m_pMusic->IsPlaying() == false) m_pMusic->Play();
+
+    m_pMusic->SetTriggerParameter(PLAY_TRIGGER_NAME);
+}
---
+++ Source/ProceduralMusic/Private/ProceduralMusicGameMode.h
@@ -0,0 +1,87 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MusicGenerationSpecs.h"
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralMusicGameMode.generated.h"
+
+class UDrumAndBassGenerator;
+class UAudioComponent;
+
+struct FMusicNote;
+struct FChordArrays;
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class AProceduralMusicGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+	AProceduralMusicGameMode();
+
+private:
+	void BeginPlay() override;
+
+	void SetDrumPatterns(TArray<float> a_KickPattern, TArray<float> a_SnarePattern, TArray<float> a_HHPattern, TArray<float> a_Perc1Pattern, TArray<float> a_Perc2Pattern);
+	void SetBassPatterns(TArray<FMusicNote> a_BassNotes);
+	void SetChordPatterns(FChordArrays a_Chords);
+	void SetMelodyPatterns(TArray<FMusicNote> a_MelodyNotes);
+
+	TArray<TArray<float>> SplitNoteArray(TArray<FMusicNote> a_NotePattern);
+
+	void PlayMusic();
+
+public:
+	UPROPERTY(EditAnywhere)
+	int32 m_Seed { 420 };
+	
+	UPROPERTY(EditAnywhere)
+	FMusicGenerationSpecs m_MusicGenSpecs
+	{
+		false,
+		false,
+		false,
+		Genre::DrumAndBass,
+		MusicSpeed::Slow,
+		MusicEnergy::Low
+	};
+private:
+
+	UPROPERTY(VisibleAnywhere, DisplayName = "Drum And Bass Generator", Category = "Music Generation")
+	TObjectPtr<UDrumAndBassGenerator> m_pDnBGenerator = nullptr;
+
+	UPROPERTY(EditAnywhere, DisplayName = "Music", Category = "Playback")
+	TObjectPtr<UAudioComponent> m_pMusic = nullptr;
+
+private:
+	const FString META_SOUND_PATH = "/Script/MetasoundEngine.MetaSoundSource'/Game/Audio/MetaSounds/META_DrumAndBass.META_DrumAndBass'";
+
+	const FName PLAY_TRIGGER_NAME = "PlayMusic";
+
+	const FName KICK_PATTERN_NAME = "KickPattern";
+	const FName SNARE_PATTERN_NAME = "SnarePattern";
+	const FName HH_PATTERN_NAME = "HHPattern";
+	const FName PERC1_PATTERN_NAME = "Perc1Pattern";
+	const FName PERC2_PATTERN_NAME = "Perc2Pattern";
+
+	const FName BASS_NOTES_PATTERN_NAME = "BassNotes";
+	const FName BASS_VELOCITIES_PATTERN_NAME = "BassVelocities";
+
+	const FName CHORD_ROOT_NOTES_PATTERN_NAME = "ChordRoots"; 
+	const FName CHORD_ROOT_VELOCITIES_PATTERN_NAME = "ChordRootVelocities"; 
+	const FName CHORD_THIRD_NOTES_PATTERN_NAME = "Chord3rds"; 
+	const FName CHORD_THIRD_VELOCITIES_PATTERN_NAME = "Chord3rdVelocities"; 
+	const FName CHORD_FIFTH_NOTES_PATTERN_NAME = "Chord5ths"; 
+	const FName CHORD_FIFTH_VELOCITIES_PATTERN_NAME = "Chord5thVelocities"; 
+	const FName CHORD_SEVENTH_NOTES_PATTERN_NAME = "Chord7ths"; 
+	const FName CHORD_SEVENTH_VELOCITIES_PATTERN_NAME = "Chord7thVelocities"; 
+
+	const FName MELODY_NOTES_PATTERN_NAME = "MelodyNotes";
+	const FName MELODY_VELOCITIES_PATTERN_NAME = "MelodyVelocities";
+};
